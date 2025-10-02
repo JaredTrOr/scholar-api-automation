7a41e17f05e5d31cff77c40758ae0d5789d47705
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2025-10-01 21:46:12 -0600

    Creation of specific exceptions for Http requests

---
+++ src/main/java/org/jared/trujillo/exceptions/HttpException.java
@@ -0,0 +1,11 @@
+package org.jared.trujillo.exceptions;
+
+public class HttpException extends RuntimeException {
+    public HttpException(String message) {
+        super(message);
+    }
+
+    public HttpException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
