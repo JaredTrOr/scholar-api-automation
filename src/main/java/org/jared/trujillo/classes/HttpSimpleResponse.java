d180bf1ac6e72735ff9256b9dc804fcdc4bfc1a0
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2025-10-01 21:48:02 -0600

    Creation of HttpClient interface and HttpSimpleResponse class to handle repsonses properly and simple.

---
+++ src/main/java/org/jared/trujillo/classes/HttpSimpleResponse.java
@@ -0,0 +1,50 @@
+package org.jared.trujillo.classes;
+
+import java.util.Map;
+
+public class HttpSimpleResponse {
+
+    private int statusCode;
+    private String body;
+    private Map<String, String> headers;
+    private String reasonPhrase;
+
+    public HttpSimpleResponse(int statusCode, String body, Map<String, String> headers, String reasonPhrase) {
+        this.statusCode = statusCode;
+        this.body = body;
+        this.headers = headers;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public HttpSimpleResponse(int statusCode, String body, Map<String, String> headers) {
+        this.statusCode = statusCode;
+        this.body = body;
+        this.headers = headers;
+    }
+
+    public int getStatusCode() {
+        return this.statusCode;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public String getReasonPhrase() {
+        return this.reasonPhrase;
+    }
+
+    @Override
+    public String toString() {
+        return "HttpSimpleResponse{" +
+                "statusCode=" + statusCode +
+                ", body='" + body + '\'' +
+                ", headers=" + headers.toString() +
+                ", reasonPhrase='" + reasonPhrase + '\'' +
+                '}';
+    }
+}
