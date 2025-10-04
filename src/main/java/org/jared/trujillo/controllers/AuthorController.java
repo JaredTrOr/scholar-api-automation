cef9536ae49d946133ebbe17e6678e67e417b66b
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2025-10-04 17:36:40 -0600

    Creation of author controller to bring search data

--- src/main/java/org/jared/trujillo/controllers/AuthorController.java
+++ src/main/java/org/jared/trujillo/controllers/AuthorController.java
@@ -2,42 +2,29 @@ package org.jared.trujillo.controllers;
 
 import org.jared.trujillo.classes.HttpClientFactory;
 import org.jared.trujillo.classes.HttpSimpleResponse;
-import org.jared.trujillo.classes.ParsedGoogleScholarData;
+import org.jared.trujillo.models.ParsedGoogleScholarData;
+import org.jared.trujillo.exceptions.JsonHandlerException;
 import org.jared.trujillo.handlers.JacksonHandler;
 import org.jared.trujillo.interfaces.HttpClient;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class AuthorController {
-
-    final String API_KEY = System.getenv("api_key");
-    final String[] ENGINES = { "google_scholar", "google_scholar_author" };
-    String query = "john";
-    String restriction = "{search_metadata, search_information, profiles.authors, organic_results, serpapi_pagination}";
-    final String API_URL = "https://serpapi.com/search?engine="
-            +this.ENGINES[0]
-            +"&api_key="+this.API_KEY
-            +"&q=author:"+ URLEncoder.encode(query, StandardCharsets.UTF_8)
-            +"&json_restrictor="+URLEncoder.encode(restriction, StandardCharsets.UTF_8);
-            /*+"&start="+0
-            +"&num="+3;*/
 
     HttpClient http = HttpClientFactory.createHttpClient();
     JacksonHandler jsonHandler = new JacksonHandler();
 
     public AuthorController() { }
 
-    public void getData() {
+    public ParsedGoogleScholarData searchByAuthorAndTopic(String apiUrl) {
         try {
-            HttpSimpleResponse response = this.http.get(this.API_URL);
-            ParsedGoogleScholarData data = jsonHandler.parseGoogleScholarResponse(response.getBody());
+            HttpSimpleResponse response = this.http.get(apiUrl);
 
-            // Send data to views
+            return jsonHandler.parseGoogleScholarResponse(response.getBody());
         } catch(Exception e) {
-            System.out.println(e);
+            throw new JsonHandlerException(e.getMessage());
         }
-
     }
+
+
 
 }
