531945230d25f89882e6880579bff32c9fc2df8a
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2025-10-04 17:29:34 -0600

    Api url builder to add search parameters

---
+++ src/main/java/org/jared/trujillo/handlers/ApiUrlBuilder.java
@@ -0,0 +1,37 @@
+package org.jared.trujillo.handlers;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class ApiUrlBuilder {
+
+    private static final String API_KEY = System.getenv("api_key");
+    private static final String BASE_API_URL = "https://serpapi.com/search";
+
+    public static String buildUrl(String engine, String restrictions, Map<String, String> parameters) {
+
+        StringBuilder urlBuilder = new StringBuilder(BASE_API_URL);
+
+        // BASE URL
+        urlBuilder.append("?engine=").append(engine);
+        urlBuilder.append("&api_key=").append(API_KEY);
+        urlBuilder.append("&json_restrictor=").append(URLEncoder.encode(restrictions, StandardCharsets.UTF_8));
+
+        try {
+
+            for (Map.Entry<String, String> param : parameters.entrySet()) {
+                urlBuilder.append("&");
+                urlBuilder.append(param.getKey());
+                urlBuilder.append("=");
+                urlBuilder.append(URLEncoder.encode(param.getValue(), StandardCharsets.UTF_8));
+            }
+
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return urlBuilder.toString();
+    }
+
+}
